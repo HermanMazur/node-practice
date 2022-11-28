d19871fdde44b7c1eef1d7a3e3a9b57a4f7f14b2
color <95304946+HermanMazur@users.noreply.github.com>
2022-11-28 13:53:28 +0200

    123

--- dateUtils.js
+++ dateUtils.js
@@ -1,6 +1,8 @@
 function getCurrentDate() {
-return Date.now()
+    return Date.now() + global.testData;
 }
+
+console.log(__filename); 
 
 module.exports = {
     getCurrentDate
--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const { getCurrentDate } = require('./dateUtils'); 
-console.log(`get current date function result: ${getCurrentDate()}`);     
+// const { getCurrentDate } = require('./dateUtils');
+// // global.testData =
+// console.log(process.argv);
+// // console.log(process.env);
+// // console.log(global.testData)
+// // process.exit()
+
+// console.log(211312313)
+
+// console.log(__dirname);
+// // console.log(__filename);
+
+
+const Calc = require('calc-js').Calc; 
+
+console.log(process.argv);
+const [, , a, b] = process.argv;
+
+console.log(new Calc(parseInt(a)).sum(parseInt(b)).finish());
